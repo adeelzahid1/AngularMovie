07a150bf34f657480fe0c992877c6a078ee0c2a8
AdeelZahid1 <developer.adeelzahid@gmail.com>
2023-02-26 23:55:12 +0500

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.persons =   [
+        {
+          name: 'adeel',
+          age: 26,
+          interest: 'programming',
+          dob: new Date(),
+          salery: 299.2000,
+        },
+        {
+          name: 'mohsin',
+          age: 27,
+          interest: 'database',
+          dob: '2022-10-01',
+          salery: 200.2000,
+        },
+        ];
+    }, 200);
+  }
+
+  persons: any | null;
+
   title = 'AngularMovie';
 
-  persons =   [
-  {
-    name: 'adeel',
-    age: 26,
-    interest: 'programming',
-    dob: new Date()
-  },
-  {
-    name: 'mohsin',
-    age: 27,
-    interest: 'database',
-    dob: '2022-10-01'
-  },
 
-  ];
 
 
 }
