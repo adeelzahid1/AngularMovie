f6eccbc6db5bf33172e626e3255c70f4742dbb2f
AdeelZahid1 <developer.adeelzahid@gmail.com>
2023-02-27 01:15:31 +0500

    passing parameter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,31 +8,48 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.persons =   [
-        {
-          name: 'adeel',
-          age: 26,
-          interest: 'programming',
-          dob: new Date(),
-          salery: 299.2000,
-        },
-        {
-          name: 'mohsin',
-          age: 27,
-          interest: 'database',
-          dob: '2022-10-01',
-          salery: 200.2000,
-        },
-        ];
-    }, 200);
+   setTimeout(() => {
+    this.moviesInTheaters = [
+      {
+        name: 'spider-man',
+        age: 26,
+        interest: 'programming',
+        dob: new Date(),
+        salery: 209.2000,
+      },
+      {
+        name: 'golmal',
+        age: 27,
+        interest: 'database',
+        dob: '2022-10-01',
+        salery: 200.2000,
+      },
+    ]
+
+    this.moviesInFutureRelease = [
+      {
+        name: 'golman 2',
+        age: 26,
+        interest: 'programming',
+        dob: new Date(),
+        salery: 209.2000,
+      },
+      {
+        name: 'ok 2',
+        age: 27,
+        interest: 'database',
+        dob: '2022-10-01',
+        salery: 200.2000,
+      },
+    ]
+   }, 200);
   }
 
-  persons: any | null;
+
 
   title = 'AngularMovie';
-
-
+  moviesInTheaters : any | null;
+  moviesInFutureRelease : any  | null;
 
 
 }
--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.css']
 })
-export class MoviesListComponent {
+export class MoviesListComponent implements OnInit{
+
+  ngOnInit(): void {
+  }
+
+  @Input()
+
+  movies: any | null;
 
 }
