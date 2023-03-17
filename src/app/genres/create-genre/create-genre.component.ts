3a2f43c3feb0741275316067239fef9649dba3c1
AdeelZahid1 <developer.adeelzahid@gmail.com>
2023-03-18 01:54:26 +0500

    auto load resolve

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,6 +1,7 @@
 import { Component,  OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { genreCreationDTO } from '../genres.model';
+import { GenresService } from '../genres.service';
 
 @Component({
   selector: 'app-create-genre',
@@ -9,16 +10,50 @@ import { genreCreationDTO } from '../genres.model';
 })
 export class CreateGenreComponent implements OnInit{
 
-constructor(private router: Router){}
+
+ constructor(private router: Router, private genreService: GenresService){}
 ngOnInit(): void {}
 
 SaveChanges(genreCreationDTO: genreCreationDTO){
-  // save genre : Todo
+  debugger;
   console.log('save changes');
-  console.log(genreCreationDTO.name);
-  this.router.navigate(['/genres']);
+  console.warn(genreCreationDTO);
+  this.genreService.createGenre(genreCreationDTO).subscribe(
+     {
+       next: () => { },
+       error : (error) => {console.warn(error)},
+       complete : () => {
+        console.log("Record Added Successfully ...");
+        this.router.navigate(['/genres']);
+       },
+        // doSomethingElse : ()
+      },
+    );
+
 }
 
 
 
+
 }
+
+
+
+
+
+
+// return {
+//   connect: (stream$: Observable<T>) => {
+//     innerStream$ = stream$.pipe(takeUntil(unsub$));
+//     return context;
+//   },
+//   subscribe(
+//     next?: (value: T) => void,
+//     error?: (e: any) => void,
+//     complete?: () => void
+//   ): Subscription {
+//     return innerStream$
+//       .pipe(takeUntil(unsubscribe$))
+//       .subscribe(next, error, complete);
+//   },
+// };
